652a476e68981d9111173bda3ee0320812161f49
unknown <aashusmaileforever@gmail.com>
2018-05-23 11:10:27 +0530

    add the background image to body  background

--- main.js
+++ main.js
@@ -10,7 +10,6 @@ function playSoundFunction(e) {
 };
 
 
-
 function removeTransitionFunction(e) {
     if (e.propertyName !== 'transform') return; // skip it if it's not a transform
     this.classList.remove('active');
